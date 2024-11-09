9a4e086ee7de000e3d70664595f05aa24b594131
Carlos Patino <carlospatinos@gmail.com>
2024-11-09 20:10:03 +0000

    Initial changes for jwt

--- src/main/java/com/descentrilizedsynergy/supdem/consumer/controller/ConsumerApiController.java
+++ src/main/java/com/descentrilizedsynergy/supdem/consumer/controller/ConsumerApiController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.descentrilizedsynergy.supdem.consumer.model.ConsumerProfileRequest;
@@ -19,6 +20,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Slf4j
+@RequestMapping("/consumer")
 public class ConsumerApiController {
     @Autowired
     private ConsumerProfileService service;
--- src/main/java/com/descentrilizedsynergy/supdem/consumer/db/repository/ConsumerProfileRepository.java
+++ src/main/java/com/descentrilizedsynergy/supdem/consumer/db/repository/ConsumerProfileRepository.java
@@ -1,16 +1,15 @@
 package com.descentrilizedsynergy.supdem.consumer.db.repository;
 
-import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.descentrilizedsynergy.supdem.consumer.db.model.ConsumerProfile;
 
 @Repository
 public interface ConsumerProfileRepository extends JpaRepository<ConsumerProfile, UUID> {
-    @Query(value = "SELECT g.* FROM {h-schema}consumer_profile g WHERE ST_Intersects(g.covered_area, ST_SetSRID(ST_MakePoint(:lng, :lat), 4326))", nativeQuery = true)
-    public List<ConsumerProfile> findAllByLngLat(Double lng, Double lat);
+    public Optional<ConsumerProfile> findByEmail(String email);
+
 }
